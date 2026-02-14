708ea7c1f36f4bb3ea9123b9b4e401634b521738
Tran Anh Huy <anhhuyrubic@gmail.com>
2026-02-14 22:22:12 +0700

    feat(config): Add GROQ API key and model to configuration

--- utils/config.py
+++ utils/config.py
@@ -7,4 +7,7 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL")
 CHAT_MODEL = os.getenv("CHAT_MODEL")
 
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+CHAT_MODEL_GROQ = os.getenv("CHAT_MODEL_GROQ")
+
 TOP_K = 5
