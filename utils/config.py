cef1006da7ce9bfc0229939656c4eb315afe99f5
Tran Anh Huy <anhhuyrubic@gmail.com>
2026-02-22 11:03:12 +0700

    fix(config): Reorder GOOGLE_API_KEY assignment for consistency

--- utils/config.py
+++ utils/config.py
@@ -3,8 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL")
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 CHAT_MODEL = os.getenv("CHAT_MODEL")
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
